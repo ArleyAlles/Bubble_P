a3ea984df42e5acd411ed7071217ece08070b36c
ArleyAlles <arley_alles@hotmail.com>
2025-04-01 10:51:33 -0300

    Adjustments on the main.py file

--- main.py
+++ main.py
@@ -12,4 +12,12 @@ T = 323.15
 x = Points().Propane_Butane()
 P_exp, y_exp = Exp_output().Propane_Butane(T)
 R, Tc, Pc, w, Kij, Psat = Parameters().Propane_Butane(T)
-Bubble_Point(T, x, R, Tc, Pc, w, Kij, Psat, tol=1e-6, n_ite=2000).AARD(P_exp,y_exp)
+
+#Object
+bubble = Bubble_Point(T, x, R, Tc, Pc, w, Kij, Psat, tol=1e-6, n_ite=2000)
+
+#Pressure and liquid molar fraction calculation
+bubble.bubble_P()
+
+#Deviation calculation
+#bubble.AARD(P_exp,y_exp)
